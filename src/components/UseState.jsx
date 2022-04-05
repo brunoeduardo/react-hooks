358abc70257c6ae567d19baa6f71660c8ee7fce3
Bruno Eduardo <bruno@beduardo.com>
2022-04-05 16:12:57 -0300

    react hooks - useState

---
+++ src/components/UseState.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+export default function UseState() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <h3>Couter</h3>
+      Value: {count}
+      <br />
+      <br />
+      <button onClick={increment}>Count+</button>
+      <br />
+      <br />
+      {/* simple form to call setCount */}
+      <button onClick={() => setCount(count + 1)}>Count+</button>
+    </div>
+  );
+}
