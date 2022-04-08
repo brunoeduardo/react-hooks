a9b70d78a2930256e6cdac514b2b8cd0e3ff3b72
Bruno Eduardo <bruno@beduardo.com>
2022-04-08 08:41:24 -0300

    update and remove item from array

--- src/components/UseStateArray.jsx
+++ src/components/UseStateArray.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 export default function UseStateArray() {
-  const listStyle = { height: "50px", overflow: "auto" };
+  const listStyle = { height: "150px", overflow: "auto" };
 
   const [inputValue, setInputValue] = useState("");
   const [arrayList, setArrayList] = useState([]);
@@ -10,11 +10,23 @@ export default function UseStateArray() {
     if (!inputValue) return;
 
     const value = {
-      id: arrayList.length + 1,
+      id: Math.random(),
       text: inputValue,
     };
 
     setArrayList([value, ...arrayList]);
+  };
+
+  const remove = (id) => {
+    setArrayList(arrayList.filter((e) => e.id !== id));
+  };
+
+  const update = (item) => {
+    setArrayList(
+      arrayList.map((el) =>
+        el.id === item.id ? { id: el.id, text: `${el.text} - edited` } : el
+      )
+    );
   };
 
   const addItem = (e) => {
@@ -28,12 +40,17 @@ export default function UseStateArray() {
       <input type="text" name="item" onChange={(e) => addItem(e)} />
       <br />
       <br />
-      <input type="button" value="Add" onClick={submit} />
+      <button onClick={submit}>Add</button>
       <br />
       <br />
       <ul style={listStyle}>
         {arrayList.map((e) => {
-          return <li key={e.id}>{e.text}</li>;
+          return (
+            <li key={e.id}>
+              {e.text} <br /> <button onClick={() => update(e)}>Update</button>
+              <button onClick={() => remove(e.id)}>Remove</button>
+            </li>
+          );
         })}
       </ul>
     </div>
